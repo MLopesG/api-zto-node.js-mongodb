e61dbc6eac3d1ed3754ef353f8ffc4dfeb549230
MLopesG <marcoslopes5687@gmail.com>
2019-11-04 00:36:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const server = require('./config/server.js');
 
-server.listen(3000, () => {
-	console.log('Servidor inicializado na porta:3000');
+server.listen(process.env.PORT || 3000, () => {
+	console.log('Servidor inicializado na porta.');
 });
